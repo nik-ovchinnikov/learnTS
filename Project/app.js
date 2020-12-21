5d927cca1ae6c33ddfc192857a1686552b45bbea
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-21 13:52:13 +0300

    complete 2.2

--- Project/app.js
+++ Project/app.js
@@ -1,1 +1,7 @@
-console.log("Your code is going to be written here ...");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.4;
+var result = add(number1, number2);
+console.log(result);
