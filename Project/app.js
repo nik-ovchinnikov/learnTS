05b262ed5b126d1344498695099bb4e32eabaab1
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 12:26:17 +0300

    pass lesson 2-5

--- Project/app.js
+++ Project/app.js
@@ -1,7 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, printPhrase) {
+    if (showResult)
+        console.log(printPhrase + n1 + n2);
+    else
+        return n1 + n2;
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var printPhrase = "The result is: ";
+var result = add(number1, number2, printResult, printPhrase);
 console.log(result);
--- Project/app.ts
+++ Project/app.ts
@@ -1,10 +1,12 @@
-function add (n1: number, n2: number) {
-    console.log(typeof number1);
-    return n1 + n2;
+function add (n1: number, n2: number, showResult: boolean, printPhrase: string) {
+    if(showResult) console.log(printPhrase + n1 + n2);
+    else return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const printPhrase = "The result is: "
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult, printPhrase);
 console.log(result);
