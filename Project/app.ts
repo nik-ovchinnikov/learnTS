2bbbe53b513a1dd1363ed3802fdf16eff14587cc
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 15:46:22 +0300

    unknown type

--- Project/app.ts
+++ Project/app.ts
@@ -1,24 +1,7 @@
-function add(number1: number, number2: number): string {
-    return (number1 + number2).toString();
+let userInput: unknown;
+let userName: string;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-const n1 = 3;
-const n2 = 34;
-
-console.log(add(n1, n2));
-function showRes(num: number): void {
-    console.log(num);
-}
-// showRes(+add(3, 32));
-
-let combinedValues: Function;
-let combinedValues2: (a: number, b: number) => number;
-
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-
-}
-
-addAndHandle(34, 34, (num) => {console.log(num);});
