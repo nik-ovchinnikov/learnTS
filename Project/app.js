c068c4a3d141b398e286c82ecaf22b500dc510ce
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 15:33:58 +0300

    callbacks lesson 2-21

--- Project/app.js
+++ Project/app.js
@@ -7,6 +7,12 @@ console.log(add(n1, n2));
 function showRes(num) {
     console.log(num);
 }
-showRes(+add(3, 32));
-var combinedValues = add;
-console.log(combinedValues(8, 8));
+// showRes(+add(3, 32));
+var combinedValues;
+var combinedValues2;
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+// console.log(combinedValues(8, 8));
+addAndHandle(34, 34, function (num) { console.log(num); });
--- Project/app.ts
+++ Project/app.ts
@@ -9,13 +9,16 @@ console.log(add(n1, n2));
 function showRes(num: number): void {
     console.log(num);
 }
-showRes(+add(3, 32));
+// showRes(+add(3, 32));
 
 let combinedValues: Function;
 let combinedValues2: (a: number, b: number) => number;
 
 
-combinedValues = add;
-combinedValues = showRes;
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
 
-console.log(combinedValues(8, 8));
+}
+
+addAndHandle(34, 34, (num) => {console.log(num);});
