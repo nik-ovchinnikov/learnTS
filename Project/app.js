ad784317ea21cffc9869eadf7aad9524a2cd86bf
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 13:03:33 +0300

    pass arrays 2-10

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: "Niki",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Tennis']
 };
+var fovoriteActivities;
+fovoriteActivities = ['Sport'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- Project/app.ts
+++ Project/app.ts
@@ -1,9 +1,18 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
     name: "Niki",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Tennis']
 };
 
+let fovoriteActivities: any[];
+fovoriteActivities = ['Sport'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
