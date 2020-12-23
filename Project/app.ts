92938acb463f9f3cada2873ff9a7739f54135b43
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 12:11:29 +0300

    after 3-2

--- Project/app.ts
+++ Project/app.ts
@@ -1,11 +1,10 @@
-function add(n1: number, n2: number) {
+function add (n1: number, n2: number) {
+    console.log(typeof number1);
     return n1 + n2;
 }
 
 const number1 = 5;
-const type = typeof number1;
-console.log(type);
-const number2 = 2.4;
+const number2 = 2.8;
 
 const result = add(number1, number2);
 console.log(result);
