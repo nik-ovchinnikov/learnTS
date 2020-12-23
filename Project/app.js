2f4e34caef48a6511c2988ad879d7688c921dc4c
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-23 13:28:29 +0300

    pass tuples in ts lesson 2-11

--- Project/app.js
+++ Project/app.js
@@ -1,16 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
 var person = {
+    // const person = {
     name: "Niki",
     age: 30,
-    hobbies: ['Sports', 'Tennis']
+    hobbies: ['Sports', 'Tennis'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+person.role[1] = "admin";
+console.log(person);
 var fovoriteActivities;
 fovoriteActivities = ['Sport'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
 }
--- Project/app.ts
+++ Project/app.ts
@@ -1,13 +1,19 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+// const person = {
     name: "Niki",
     age: 30,
-    hobbies: ['Sports', 'Tennis']
+    hobbies: ['Sports', 'Tennis'],
+    role: [2, 'author']
 };
 
+person.role.push('admin');
+person.role[1] = "admin";
+console.log(person);
 let fovoriteActivities: any[];
 fovoriteActivities = ['Sport'];
 
