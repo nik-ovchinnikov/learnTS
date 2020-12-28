07238e954ba38fa54ea0579827f42d928453da29
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-28 07:24:24 +0300

    public and private modificators

--- 5 - Classes/dist/app.js
+++ 5 - Classes/dist/app.js
@@ -1,14 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = []; //now there is no access to rhis field from outside
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department("Accounting");
-accounting.describe();
-const copyAccountung = { name: "s", describe: accounting.describe };
-copyAccountung.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- 5 - Classes/src/app.ts
+++ 5 - Classes/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];//now there is no access to rhis field from outside
 
     constructor(n: string) {
        this.name = n; 
@@ -8,12 +9,20 @@ class Department {
     describe(this: Department ) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
 
-accounting.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 
-const copyAccountung = {name: "s", describe: accounting.describe};
-
-copyAccountung.describe();
+accounting.printEmployeeInformation();
